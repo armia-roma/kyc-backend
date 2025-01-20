a38b2298e01173a53e914d3354590c1ac908d306
armia-roma <armia_bos@gmail.com>
2025-01-20 22:54:55 +0400

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// we’ll install the @types declaration packages for Express and Node.js, which offer type definitions in the form of declaration files.
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+	console.log(`[server]: Server is running at http://localhost:${port}`);
+});
