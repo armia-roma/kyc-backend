b7432064bd5359beaffcad9ade8611f0101e5685
armia-roma <armia_bos@gmail.com>
2025-01-21 15:38:08 +0400

    add login method to user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -31,5 +31,24 @@ class UserController {
 			res.status(400).json(responseEntity);
 		}
 	}
+	async login(req: Request, res: Response) {
+		try {
+			const { email, password } = req.body;
+			const token = UserService.login(email, password);
+			responseEntity.status = 200;
+			responseEntity.message = "Login successful";
+			responseEntity.data = { token };
+			responseEntity.error = null;
+
+			return res.status(200).json(responseEntity);
+		} catch (error: any) {
+			responseEntity.status = 400;
+			responseEntity.message = error.message;
+			responseEntity.data = null;
+			responseEntity.error = error;
+
+			return res.status(400).json(responseEntity);
+		}
+	}
 }
 export default new UserController();
