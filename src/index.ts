6cf7f73bf00a9c4a7025826e8bbf2101697c38a7
armia-roma <armia_bos@gmail.com>
2025-01-21 14:28:13 +0400

    add db connect , auth route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 // we’ll install the @types declaration packages for Express and Node.js, which offer type definitions in the form of declaration files.
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-
+import { dbConnect } from "./config/dbConnect";
+import authRoutes from "./routes/AuthRoutes";
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+dbConnect();
+app.use(express.json());
+app.use("/api/user", authRoutes);
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Express + TypeScript Server");
