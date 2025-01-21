dc3e1c56edc4fe21878cd997db360558e98a3bd8
armia-roma <armia_bos@gmail.com>
2025-01-21 14:29:26 +0400

    add auth route

---
+++ src/routes/AuthRoutes.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from "express";
+import { register } from "module";
+import UserController from "../controllers/UserController";
+import authValidation from "../validations/AuthValidation";
+const router = express.Router();
+
+router.post(
+	"/register",
+	authValidation.validateRegistration(),
+	UserController.register
+);
+
+export default router;
