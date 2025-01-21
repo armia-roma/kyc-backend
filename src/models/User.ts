5b5b9a39ab69d82106f6cab996a8ee29a5d12774
armia-roma <armia_bos@gmail.com>
2025-01-21 14:05:38 +0400

    add user model

---
+++ src/models/User.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+// Define the User interface
+enum Role {
+	Admin = "admin",
+	User = "user",
+}
+interface IUser extends Document {
+	userName: string;
+	email: string;
+	password: string;
+	role: Role;
+}
+
+// Define the User schema
+const userSchema: Schema = new Schema({
+	userName: { type: String, required: true, unique: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	role: {
+		type: String,
+		required: true,
+		enum: ["admin", "user"],
+	},
+});
+
+// Create the User model
+const User = mongoose.model<IUser>("User", userSchema);
+
+export { User, IUser };
