08e9df82b44ebda739fc68766fe236a69f57180e
armia-roma <armia_bos@gmail.com>
2025-01-23 10:06:46 +0400

    clean up

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -1,5 +1,4 @@
 import express, { Express, Request, Response } from "express";
-import { register } from "module";
 import UserController from "../controllers/UserController";
 import authValidation from "../validations/AuthValidation";
 const router = express.Router();
