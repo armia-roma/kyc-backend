fbc73bbaa84d216e62ffcf8159047f90d82e55e5
armia-roma <armia_bos@gmail.com>
2025-01-23 10:07:55 +0400

    add kyc route

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,19 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { dbConnect } from "./config/dbConnect";
 import authRoutes from "./routes/AuthRoutes";
+import kycRoutes from "./routes/KycRoutes";
+import cors from "cors";
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 dbConnect();
+app.use(cors());
 app.use(express.json());
 app.use("/api/user", authRoutes);
-
+app.use("/uploads", express.static("uploads"));
+app.use("/api/kyc", kycRoutes);
 app.get("/", (req: Request, res: Response) => {
 	res.send("Express + TypeScript Server");
 });
