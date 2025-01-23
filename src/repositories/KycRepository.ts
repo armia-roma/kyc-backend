1f88fee037f81a15df5e9c8ef31d7d66b499644c
armia-roma <armia_bos@gmail.com>
2025-01-24 02:19:03 +0400

    add list method to kyc repository

--- src/repositories/KycRepository.ts
+++ src/repositories/KycRepository.ts
@@ -1,7 +1,10 @@
-import { Kyc, IKyc } from "../models/Kyc";
+import {Kyc, IKyc} from "../models/Kyc";
 class KycRepository {
 	async create(kyc: IKyc): Promise<IKyc> {
 		return await Kyc.create(kyc);
+	}
+	async list(): Promise<IKyc[]> {
+		return await Kyc.find();
 	}
 }
 export default new KycRepository();
