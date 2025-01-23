3d06164f55c2b06a989e093e079f77778a277472
armia-roma <armia_bos@gmail.com>
2025-01-23 18:20:30 +0400

    export Role interface

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,7 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, {Document, Schema} from "mongoose";
 
 // Define the User interface
-enum Role {
+export enum Role {
 	Admin = "admin",
 	User = "user",
 }
@@ -14,9 +14,9 @@ interface IUser extends Document {
 
 // Define the User schema
 const userSchema: Schema = new Schema({
-	userName: { type: String, required: true, unique: true },
-	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
+	userName: {type: String, required: true, unique: true},
+	email: {type: String, required: true, unique: true},
+	password: {type: String, required: true},
 	role: {
 		type: String,
 		required: true,
@@ -27,4 +27,4 @@ const userSchema: Schema = new Schema({
 // Create the User model
 const User = mongoose.model<IUser>("User", userSchema);
 
-export { User, IUser };
+export {User, IUser};
