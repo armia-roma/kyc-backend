b8c86bdbae09f229f4896f25d65b7e6278ca8791
armia-roma <armia_bos@gmail.com>
2025-01-23 17:53:26 +0400

    add create kyc  to  kyc  service

--- src/services/KycService.ts
+++ src/services/KycService.ts
@@ -1,22 +1,32 @@
-import { IKyc } from "../models/Kyc";
+import {IKyc} from "../models/Kyc";
 import KycRepository from "./../repositories/KycRepository";
 class KycService {
-	async create(
-		full_name: string,
-		phone_number: string,
-		address: string,
-		email: string,
-		file_path: string
-	) {
-		const kyc = await KycRepository.create({
-			full_name,
-			phone_number,
-			address,
-			email,
-			file_path,
-			status: "pending", // first status
-		} as IKyc);
-		return kyc;
+	async create({
+		full_name,
+		phone_number,
+		address,
+		email,
+		file_path,
+		user,
+	}: Partial<IKyc>) {
+		try {
+			const kyc = await KycRepository.create({
+				full_name,
+				phone_number,
+				address,
+				email,
+				file_path,
+				status: "pending",
+				user,
+			} as IKyc);
+			return kyc;
+		} catch (error: any) {
+			if (error.name === "ValidationError") {
+				throw new Error(`Validation error: ${error.message}`);
+			}
+
+			throw new Error("Failed to create KYC record");
+		}
 	}
 }
 export default new KycService();
