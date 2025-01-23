0105af712e5623fa071315b24245fc65abbade89
armia-roma <armia_bos@gmail.com>
2025-01-24 02:23:49 +0400

    edit add  user to  register response

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,18 +1,22 @@
-import { Request, Response, NextFunction } from "express";
+import {Request, Response, NextFunction} from "express";
 import UserService from "../services/UserService";
 import responseEntity from "../../utils/ResponseEntity";
 
 class UserController {
 	async register(req: Request, res: Response, next: NextFunction) {
 		try {
-			const { userName, email, password, role } = req.body;
+			const {userName, email, password, role} = req.body;
 			const user = await UserService.register(
 				userName,
 				email,
 				password,
 				role
 			);
-			responseEntity.setResponse(201, "User registered successfully");
+			responseEntity.setResponse(
+				201,
+				"User registered successfully",
+				user
+			);
 
 			responseEntity.error = null;
 
@@ -25,7 +29,7 @@ class UserController {
 	}
 	async login(req: Request, res: Response) {
 		try {
-			const { email, password } = req.body;
+			const {email, password} = req.body;
 			const token = await UserService.login(email, password);
 			responseEntity.setResponse(200, "Login successful", token);
 
@@ -34,6 +38,20 @@ class UserController {
 			responseEntity.setResponse(400, error.message, null, error);
 
 			res.status(400).json(responseEntity);
+		}
+	}
+	async verify(req: Request, res: Response) {
+		const token = req.body.token;
+		try {
+			const user = await UserService.verify(token);
+
+			responseEntity.setResponse(200, "valid", user, null);
+
+			res.json(responseEntity);
+			return;
+		} catch (error) {
+			res.status(401).json({error: "Invalid token"});
+			return;
 		}
 	}
 }
